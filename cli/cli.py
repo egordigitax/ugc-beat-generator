4515fd514694de0d7b5d7da6720f5ffcbe129764
egordigitax <fantomaka@gmail.com>
2023-09-04 19:54:58 +0500

    cli design complete

--- cli/cli.py
+++ cli/cli.py
@@ -12,121 +12,127 @@ def initArgParse() -> argparse.ArgumentParser:
     )
 
     misc = parser.add_argument_group("Misc Options")
-    required_named = parser.add_argument_group("Required Options")
+    required = parser.add_argument_group("Required Options")
     general = parser.add_argument_group("General Options")
     graphics = parser.add_argument_group("Graphic Rendering Options")
     music_analyzer = parser.add_argument_group("Music Analyzer Options")
 
-    # Misc
+    # Misc Options
+    def __add_misc_arguments():
+        misc.add_argument("-h", "--help", action="help", help="show this help message and exit")
 
-    misc.add_argument("-h", "--help", action="help", help="show this help message and exit")
+        misc.add_argument("--version", action="version",
+                          version=f"{parser.prog} version 1.3.0")
 
-    misc.add_argument("--version", action="version",
-                        version=f"{parser.prog} version 1.3.0")
+        misc.add_argument("--demo", required=False, default=False,
+                          action='store_true',
+                          help="for dev purposes (generates test frames or amplitudes)")
 
-    misc.add_argument("--demo", required=False, default=False,
-                              action='store_true',
-                              help="for dev purposes (generates test frames or amplitudes)")
+    # Required Options
+    def __add_required_arguments():
+        required.add_argument("-b", "--beat", help="set ups beat path (.wav)", type=str, required=True)
 
-    # Required
+        required.add_argument("-o", "--output_path",
+                                    help="path to resulting frames path",
+                                    type=str, required=True)
 
-    required_named.add_argument("-b", "--beat", help="set ups beat path (.wav)", type=str, required=True)
+        required.add_argument("-a", "--avatar_path",
+                                    help="path to avatar (.png)",
+                                    type=str, required=True)
 
-    required_named.add_argument("-o", "--output_path",
-                                help="path to resulting frames path",
-                                type=str, required=True)
+    # Graphics Options
+    def __add_graphics_arguments():
+        graphics.add_argument("--width",
+                              help="target width",
+                              type=int, default=720, required=False)
 
-    required_named.add_argument("-a", "--avatar_path",
-                                help="path to avatar (.png)",
-                                type=str, required=True)
+        graphics.add_argument("--height",
+                              help="target height",
+                              type=int, default=1280, required=False)
 
-    # Graphics
+        graphics.add_argument("--use-cpu", required=False, default=False,
+                              action='store_true', help="use cpu computing device")
 
-    graphics.add_argument("--width",
-                          help="target width",
-                          type=int, default=720, required=False)
+        graphics.add_argument("--samples",
+                              help="set number of rendering samples. more samples - better quality",
+                              required=False, default=64, type=int)
 
-    graphics.add_argument("--height",
-                          help="target height",
-                          type=int, default=1280, required=False)
+        graphics.add_argument("-t", "--template-id",
+                              help="set graphics template id. uses random when not specified",
+                              type=int, required=False)
 
-    graphics.add_argument("--use-cpu", required=False, default=False,
-                          action='store_true', help="use cpu computing device")
+        graphics.add_argument("-ot", "--overlay-template-id",
+                              help="applies cyclic overlay footage to video by id, pass -1 for random",
+                              type=int, required=False)
 
-    graphics.add_argument("--samples",
-                          help="set number of rendering samples. more samples - better quality",
-                          required=False, default=64, type=int)
+        graphics.add_argument("-f", "--framerate",
+                            help="target framerate",
+                            type=int, default=30, required=False)
 
-    graphics.add_argument("-t", "--template-id",
-                          help="set graphics template id. uses random when not specified",
-                          type=int, required=False)
+        graphics.add_argument("--shade_path",
+                            help="required in legacy mode. path to shade image (alpha, .png)",
+                            type=str, default=None, required=False)
 
-    graphics.add_argument("-ot", "--overlay-template-id",
-                          help="applies cyclic overlay footage to video by id, pass -1 for random",
-                          type=int, required=False)
+        graphics.add_argument("--blur_radius",
+                                  help="blur radius (intensity)",
+                                  type=int, default=50, required=False)
 
-    graphics.add_argument("-f", "--framerate",
-                        help="target framerate",
-                        type=int, default=30, required=False)
+        graphics.add_argument("--avatar_size",
+                                  help="avatar new size",
+                                  type=int, default=400, required=False)
 
-    graphics.add_argument("--shade_path",
-                        help="required in legacy mode. path to shade image (alpha, .png)",
-                        type=str, default=None, required=False)
+    # General Options
+    def __add_general_arguments():
+        general.add_argument("--legacy",
+                            help="uses legacy version of generator",
+                            required=False, action='store_true')
 
-    graphics.add_argument("--blur_radius",
-                              help="blur radius (intensity)",
-                              type=int, default=50, required=False)
+        general.add_argument("-j", "--jobs",
+                            help="number of parallel jobs",
+                            type=int, default=8, required=False)
 
-    graphics.add_argument("--avatar_size",
-                              help="avatar new size",
-                              type=int, default=400, required=False)
+        general.add_argument("-v", "--verbose",
+                            help="verbose computations",
+                            action='store_true', required=False, default=False)
 
-    # General
+        general.add_argument("--output_type", help="select output presentation. available types: \n "
+                                                  "* frames (--output_type=frames) \n"
+                                                  "* raw (--output_type=raw)\n",
+                            default="frames", type=str, required=False)
 
-    general.add_argument("--legacy",
-                        help="uses legacy version of generator",
-                        required=False, action='store_true')
+    # Music Analyzer Options
+    def __add_music_analyzer_arguments():
+        music_analyzer.add_argument("-s", "--smooth",
+                            help="set smooth factor by int value\nthe less -- the smoother",
+                            default=8, type=int, required=False)
 
-    general.add_argument("-j", "--jobs",
-                        help="number of parallel jobs",
-                        type=int, default=8, required=False)
+        music_analyzer.add_argument("-w", "--widening",
+                            help="set widening factor by float value\nthe less -- the smoother",
+                            default=0.5, type=float, required=False)
 
-    general.add_argument("-v", "--verbose",
-                        help="verbose computations",
-                        action='store_true', required=False, default=False)
+        music_analyzer.add_argument("--percussive_influence",
+                            help="set percussive part influence (0.0 -- 1.0)\n",
+                            default=0.5, type=float, required=False)
 
-    general.add_argument("--output_type", help="select output presentation. available types: \n "
-                                              "* frames (--output_type=frames) \n"
-                                              "* raw (--output_type=raw)\n",
-                        default="frames", type=str, required=False)
+        music_analyzer.add_argument("--harmonic_influence",
+                            help="set harmonic part influence (0.0 -- 1.0)\n",
+                            default=0.5, type=float, required=False)
 
-    # Music Analyzer
+        music_analyzer.add_argument("--percussive_margin",
+                            help="set percussive part isolation"
+                                 "\n 1.0 -- default, more -- more isolated\n",
+                            default=1.0, type=float, required=False)
 
-    music_analyzer.add_argument("-s", "--smooth",
-                        help="set smooth factor by int value\nthe less -- the smoother",
-                        default=8, type=int, required=False)
+        music_analyzer.add_argument("--harmonic_margin",
+                            help="set harmonic part isolation"
+                                 "\n 1.0 -- default, more -- more isolated\n",
+                            default=1.0, type=float, required=False)
 
-    music_analyzer.add_argument("-w", "--widening",
-                        help="set widening factor by float value\nthe less -- the smoother",
-                        default=0.5, type=float, required=False)
-
-    music_analyzer.add_argument("--percussive_influence",
-                        help="set percussive part influence (0.0 -- 1.0)\n",
-                        default=0.5, type=float, required=False)
-
-    music_analyzer.add_argument("--harmonic_influence",
-                        help="set harmonic part influence (0.0 -- 1.0)\n",
-                        default=0.5, type=float, required=False)
-
-    music_analyzer.add_argument("--percussive_margin",
-                        help="set percussive part isolation"
-                             "\n 1.0 -- default, more -- more isolated\n",
-                        default=1.0, type=float, required=False)
-
-    music_analyzer.add_argument("--harmonic_margin",
-                        help="set harmonic part isolation"
-                             "\n 1.0 -- default, more -- more isolated\n",
-                        default=1.0, type=float, required=False)
+    __add_misc_arguments()
+    __add_required_arguments()
+    __add_general_arguments()
+    __add_graphics_arguments()
+    __add_music_analyzer_arguments()
 
     return parser
 
