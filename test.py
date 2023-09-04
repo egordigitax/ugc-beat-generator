7f8516bd86e720a2ba4f8a67255a7befea94ce5c
egordigitax <fantomaka@gmail.com>
2023-09-04 23:09:04 +0500

    restruct files

--- test.py
+++
@@ -1,4 +0,0 @@
-from cli.engine.blender import BlenderEngine
-
-engine = BlenderEngine('sdk/blender/blender-3.6.2-linux-x64/blender')
-engine.render('tests/sources/project.blend', 'tests/', 1)
--- tests/test.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system('rm -rf tests/result/*')
-os.system('python cli/cli.py -b tests/sources/demo.wav -o tests/result/ -a tests/sources/demo.png --shade_path tests/sources/Shadow.png -ot 23')
